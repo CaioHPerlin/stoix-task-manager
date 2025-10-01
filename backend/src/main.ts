56f03054f94d19c6bff71716008f5456e6bbf91e
caiohperlin <caiohperlin@gmail.com>
2025-10-01 13:41:04 -0400

    chore(backend): implement api versioning

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
+import { VersioningType } from "@nestjs/common";
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
+
+    // /api
+    app.setGlobalPrefix("api");
+
+    // /api/v1
+    app.enableVersioning({
+        type: VersioningType.URI,
+        defaultVersion: "1",
+    });
     await app.listen(process.env.PORT ?? 5000);
 }
 bootstrap();
