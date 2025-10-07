ae57fbfe01bf9a77a497c1d294014854b817c0e4
caiohperlin <caiohperlin@gmail.com>
2025-10-07 11:29:29 -0400

    feat(backend): implement basic tasks CRUD

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from "@nestjs/common";
 import { HealthModule } from "./health/health.module";
 import { ConfigModule } from "./config/config.module";
-import { DatabaseModule } from './database/database.module';
+import { DatabaseModule } from "./database/database.module";
+import { TasksModule } from "./tasks/tasks.module";
 
 @Module({
-    imports: [HealthModule, ConfigModule, DatabaseModule],
+    imports: [HealthModule, ConfigModule, DatabaseModule, TasksModule],
 })
 export class AppModule {}
