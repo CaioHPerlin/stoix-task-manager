517282ca147cac47a49ecc58c2b5ebce1ff0d315
Caio H P Lima <caiohperlin@gmail.com>
2025-10-07 15:42:42 -0400

    Revert "Feature(backend)/tasks crud"

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from "@nestjs/common";
 import { HealthModule } from "./health/health.module";
 import { ConfigModule } from "./config/config.module";
-import { DatabaseModule } from "./database/database.module";
-import { TasksModule } from "./tasks/tasks.module";
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-    imports: [HealthModule, ConfigModule, DatabaseModule, TasksModule],
+    imports: [HealthModule, ConfigModule, DatabaseModule],
 })
 export class AppModule {}
